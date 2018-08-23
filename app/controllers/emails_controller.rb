4a589e12552c88202964a2ed1ea1348514bf200d
ticho <tibofolly@gmail.com>
2018-08-23 18:31:58 +0200

    undo done

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -33,7 +33,22 @@ class EmailsController < ApplicationController
 
   def delete
     @email_id = params[:id]
+    p session[:saved_object] = Email.find(params[:id]).object
+    p session[:saved_body] = Email.find(params[:id]).body
     Email.destroy(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def undo
+    @email = Email.create(
+      object: session[:saved_object],
+      body: session[:saved_body],
+      read: true
+    )
+    session[:saved_object] = nil
+    session[:saved_body] = nil
     respond_to do |format|
       format.js
     end
